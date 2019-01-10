186172104ff1a1afcdb8187593303911d3584a20
John Didion <github@didion.net>
2019-01-10 08:52:37 -0500

    add long_description to setup()

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup
 import sys
 import versioneer
@@ -10,11 +11,19 @@ if version_info < (3, 6):
     sys.exit(1)
 
 
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
+    encoding="utf-8"
+) as f:
+    long_description = f.read()
+
+
 setup(
     name='xphyle',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Utilities for working with files.',
+    long_description=long_description,
     url='https://github.com/jdidion/xphyle',
     author='John Didion',
     author_email='github@didion.net',
